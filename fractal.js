853c6d3d4a03aa1424cd0ebdc80f18d4d4cc923d
Mike Harris <m.harris@elsevier.com>
2022-10-10 07:19:41 +0100

    Add more palettes and refactor out drawing methods

--- fractal.js
+++ fractal.js
@@ -10,7 +10,16 @@
 //
 
 // set up colour palettes for colouring the levels outside the set itself
-const colour_palettes = [['#00429d', '#1448a0', '#204fa3', '#2955a6', '#315ca9', '#3862ac', '#3f69af', '#466fb2', '#4c76b5', '#527db7', '#5884ba', '#5e8abd', '#6491c0', '#6a98c2', '#709fc5', '#76a6c8', '#7cadca', '#83b4cd', '#89bbcf', '#90c2d2', '#97c9d4', '#9fd0d6', '#a7d6d8', '#afddda', '#b8e4dc', '#c2eade', '#ccf1e0', '#d9f7e1', '#e8fce1', '#ffffe0'], ['#94003a', '#98163e', '#9c2341', '#a12e45', '#a53849', '#a9414d', '#ae4951', '#b25155', '#b65959', '#ba615e', '#be6962', '#c27167', '#c6796b', '#ca8070', '#cd8874', '#d19079', '#d5977e', '#d99f83', '#dca689', '#e0ae8e', '#e3b694', '#e7bd9a', '#eac5a0', '#edcda6', '#f1d4ad', '#f4dcb4', '#f7e4bc', '#faebc5', '#fdf3cf', '#fffadf'],['#890079', '#8c197d', '#8e2881', '#903385', '#933d89', '#95478d', '#975091', '#995995', '#9b619a', '#9d699e', '#9f71a2', '#a179a6', '#a281aa', '#a489af', '#a691b3', '#a798b7', '#a9a0bc', '#aba8c0', '#adafc5', '#afb7c9', '#b1bece', '#b3c6d2', '#b6cdd7', '#b8d5dc', '#bcdce1', '#c0e3e6', '#c5eaeb', '#ccf0f1', '#d5f6f7', '#e9f9ff']]
+const colour_palettes =
+    [
+        ['#00429d', '#1448a0', '#204fa3', '#2955a6', '#315ca9', '#3862ac', '#3f69af', '#466fb2', '#4c76b5', '#527db7', '#5884ba', '#5e8abd', '#6491c0', '#6a98c2', '#709fc5', '#76a6c8', '#7cadca', '#83b4cd', '#89bbcf', '#90c2d2', '#97c9d4', '#9fd0d6', '#a7d6d8', '#afddda', '#b8e4dc', '#c2eade', '#ccf1e0', '#d9f7e1', '#e8fce1', '#ffffe0'],
+        ['#94003a', '#98163e', '#9c2341', '#a12e45', '#a53849', '#a9414d', '#ae4951', '#b25155', '#b65959', '#ba615e', '#be6962', '#c27167', '#c6796b', '#ca8070', '#cd8874', '#d19079', '#d5977e', '#d99f83', '#dca689', '#e0ae8e', '#e3b694', '#e7bd9a', '#eac5a0', '#edcda6', '#f1d4ad', '#f4dcb4', '#f7e4bc', '#faebc5', '#fdf3cf', '#fffadf'],
+        ['#890079', '#8c197d', '#8e2881', '#903385', '#933d89', '#95478d', '#975091', '#995995', '#9b619a', '#9d699e', '#9f71a2', '#a179a6', '#a281aa', '#a489af', '#a691b3', '#a798b7', '#a9a0bc', '#aba8c0', '#adafc5', '#afb7c9', '#b1bece', '#b3c6d2', '#b6cdd7', '#b8d5dc', '#bcdce1', '#c0e3e6', '#c5eaeb', '#ccf0f1', '#d5f6f7', '#e9f9ff'],
+        ['#007600', '#177b09', '#257f12', '#318419', '#3b8921', '#448e28', '#4d922f', '#569736', '#5e9c3d', '#66a144', '#6fa54c', '#77aa54', '#7faf5b', '#86b463', '#8eb86b', '#96bd74', '#9ec27c', '#a6c685', '#adcb8e', '#b5d097', '#bcd5a1', '#c4d9aa', '#cbdeb4', '#d3e3be', '#dae7c8', '#e2ecd3', '#e9f1de', '#f0f6e8', '#f8faf4', '#ffffff'],
+        ['#007600', '#257f11', '#3a8820', '#4b912d', '#5c9b3b', '#6ca449', '#7bad58', '#8bb668', '#9abf78', '#a9c889', '#b7d19a', '#c6dbad', '#d4e4c0', '#e3edd4', '#f1f6e9', '#ffeed3', '#ffdcc6', '#ffcab9', '#ffb8ab', '#ffa59e', '#fd9291', '#f78085', '#f06f7a', '#e75d6f', '#dd4c65', '#d23b5b', '#c52a52', '#b61849', '#a60741', '#93003a'],
+        ['#007ca6', '#0986ac', '#198fb2', '#2999b8', '#39a3bf', '#48acc5', '#59b5cb', '#69bed1', '#7ac7d7', '#8ccfdc', '#9ed8e2', '#b0e0e8', '#c3e8ee', '#d7f0f4', '#ebf8f9', '#ffeed3', '#ffdcc6', '#ffcab9', '#ffb8ab', '#ffa59e', '#fd9291', '#f78085', '#f06f7a', '#e75d6f', '#dd4c65', '#d23b5b', '#c52a52', '#b61849', '#a60741', '#93003a'],
+        ['#007ca6', '#0986ac', '#198fb2', '#2999b8', '#39a3bf', '#48acc5', '#59b5cb', '#69bed1', '#7ac7d7', '#8ccfdc', '#9ed8e2', '#b0e0e8', '#c3e8ee', '#d7f0f4', '#ebf8f9', '#e8f7ea', '#e3f0eb', '#dde8ed', '#d7e1ee', '#d1daef', '#cbd3f0', '#c4cbf1', '#bdc4f2', '#b7bdf2', '#afb6f3', '#a8aff3', '#a0a9f3', '#98a2f2', '#8f9cf1', '#7a9cdc']
+    ]
 
 // set up of 'screen' resolution, the size of our <canvas>
 const x_resolution = 640;
@@ -54,97 +63,89 @@ function mandelbrot() {
 
     switch (method) {
         case 'bdm':
-            mandelbrot_bdm(max_iters);
+            mandelbrot_bdm(max_iters, set_colour_bdm);
             break;
         case 'lsm':
         default:
             const palette_num = document.getElementById('palette').value;
-            mandelbrot_lsm(max_iters, palette_num);
+            mandelbrot_lsm(max_iters, set_colour_lsm);
             break;
-    }
-}
-
-// draw the mandelbrot set using the Level Set Method
-function mandelbrot_lsm(max_iters, palette_num) {
-    const canvas = document.getElementById("mset_canvas");
-    const ctx = canvas.getContext("2d");
-
-    for (let iy = 0; iy < y_resolution; iy++) {
-
-        const cy = imaginary_plan_minimum_value + iy * y_prop;
-
-        for (let ix = 0; ix < x_resolution; ix++) {
-
-            const cx = real_plane_minimum_value + ix * x_prop;
-            const point = {x: 0.0, y: 0.0};
-            const iter = compute_point(point, cx, cy, max_iters, threshold);
-
-            if (iter == max_iters) {
-                // if we didn't get to infinity by the time we
-                // used up all the iterations, then we're in the set
-                // colour it bloack
-                ctx.fillStyle = "#000000";
-            } else {
-                // otherwise colour it according to the number
-                // of iterations it took to get to infinity (threshold)
-                ctx.fillStyle = colour_palettes[palette_num][iter % colour_palettes[palette_num].length];
-            }
-            ctx.fillRect(ix, iy, 1, 1);
-        }
-    }
-}
-
-// draw the Mandelbrot Set using the Binary Decomposition Method
-function mandelbrot_bdm(max_iters) {
-    const canvas = document.getElementById("mset_canvas");
-    const ctx = canvas.getContext("2d");
-
-    for (let iy = 0; iy < y_resolution; iy++) {
-
-        const cy = imaginary_plan_minimum_value + iy * y_prop;
-
-        for (let ix = 0; ix < x_resolution; ix++) {
-
-            const cx = real_plane_minimum_value + ix * x_prop;
-            const point = {x: 0.0, y: 0.0};
-            const iter = compute_point(point, cx, cy, max_iters, threshold);
-
-            if (iter == max_iters) {
-                // if we didn't get to infinity by the time we
-                // used up all the iterations, then we're in the set
-                // colour it black
-                ctx.fillStyle = "#000000";
-            } else {
-                // color it depending on the angle of alpha
-                const alpha = Math.atan(point.y);
-                if ((alpha >= 0) && (alpha <= 3)) {
-                    ctx.fillStyle = "#fff";
-                } else {
-                    ctx.fillStyle = "#000";
-                }
-            }
-            ctx.fillRect(ix, iy, 1, 1);
-        }
     }
 }
 
 function julia() {
     const method = document.getElementById("method").value;
     const max_iters = document.getElementById('iterations').value;
+
     switch (method) {
         case 'bdm':
-            julia_bdm(max_iters);
+            julia_bdm(max_iters, set_colour_bdm);
             break;
         case 'lsm':
         default:
             const palette_num = document.getElementById('palette').value;
-            julia_lsm(max_iters, palette_num);
+            julia_lsm(max_iters, set_colour_lsm);
             break;
     }
 }
 
+function set_colour_lsm(iter, max_iters, ctx) {
+    if (iter == max_iters) {
+        // if we didn't get to infinity by the time we
+        // used up all the iterations, then we're in the set
+        // colour it bloack
+        ctx.fillStyle = "#000000";
+    } else {
+        // otherwise colour it according to the number
+        // of iterations it took to get to infinity (threshold)
+        const palette_num = document.getElementById('palette').value;
+        ctx.fillStyle = colour_palettes[palette_num][iter % colour_palettes[palette_num].length];
+    }
+}
+
+// draw the mandelbrot set using the Level Set Method
+function mandelbrot_lsm(max_iters, set_colour_func) {
+    const canvas = document.getElementById("mset_canvas");
+    const ctx = canvas.getContext("2d");
+
+    for (let iy = 0; iy < y_resolution; iy++) {
+
+        const cy = imaginary_plan_minimum_value + iy * y_prop;
+
+        for (let ix = 0; ix < x_resolution; ix++) {
+
+            const cx = real_plane_minimum_value + ix * x_prop;
+            const point = {x: 0.0, y: 0.0};
+            const iter = compute_point(point, cx, cy, max_iters, threshold);
+            set_colour_func(iter, max_iters, ctx);
+            ctx.fillRect(ix, iy, 1, 1);
+        }
+    }
+}
+
+// draw the Mandelbrot Set using the Binary Decomposition Method
+function mandelbrot_bdm(max_iters, set_colour_func) {
+    const canvas = document.getElementById("mset_canvas");
+    const ctx = canvas.getContext("2d");
+
+    for (let iy = 0; iy < y_resolution; iy++) {
+
+        const cy = imaginary_plan_minimum_value + iy * y_prop;
+
+        for (let ix = 0; ix < x_resolution; ix++) {
+
+            const cx = real_plane_minimum_value + ix * x_prop;
+            const point = {x: 0.0, y: 0.0};
+            const iter = compute_point(point, cx, cy, max_iters, threshold);
+
+            set_colour_func(iter, max_iters, ctx, point)
+            ctx.fillRect(ix, iy, 1, 1);
+        }
+    }
+}
+
 // draw a Julia set using the Level Set Method
-function julia_lsm(maxiter, palette_num) {
+function julia_lsm(max_iters, set_colour_func) {
     const canvas = document.getElementById("jset_canvas");
     const ctx = canvas.getContext("2d");
     const color_method = "lsm";
@@ -168,24 +169,32 @@ function julia_lsm(maxiter, palette_num) {
         for (let ix = 0; ix < x_resolution; ix++) {
 
             const point = {x: x_min + ix * x_prop, y: y};
-            const iter = compute_point(point, cx, cy, maxiter, threshold);
+            const iter = compute_point(point, cx, cy, max_iters, threshold);
 
-            if (iter == maxiter) {
-                // if we didn't get to infinity by the time we
-                // used up all the iterations, then we're in the set
-                // colour it black
-                ctx.fillStyle = "#000000";
-            } else {
-                // otherwise colour it according to the number
-                // of iterations it took to get to infinity (threshold)
-                ctx.fillStyle = colour_palettes[palette_num][iter % colour_palettes[palette_num].length];
-            }
+            set_colour_func(iter, max_iters, ctx, point)
             ctx.fillRect(ix, iy, 1, 1);
         }
     }
 }
 
-function julia_bdm(maxiter) {
+function set_colour_bdm(iter, maxiter, ctx, point) {
+    if (iter == maxiter) {
+        // if we didn't get to infinity by the time we
+        // used up all the iterations, then we're in the set
+        // colour it bloack
+        ctx.fillStyle = "#000000";
+    } else {
+        // color it depending on the angle of alpha
+        const alpha = Math.atan(point.y);
+        if ((alpha >= 0) && (alpha <= 3)) {
+            ctx.fillStyle = "#fff";
+        } else {
+            ctx.fillStyle = "#000";
+        }
+    }
+}
+
+function julia_bdm(max_iters, set_colour_func) {
     const canvas = document.getElementById("jset_canvas");
     const ctx = canvas.getContext("2d");
 
@@ -208,22 +217,8 @@ function julia_bdm(maxiter) {
         for (let ix = 0; ix < x_resolution; ix++) {
 
             const point = {x: x_min + ix * x_prop, y: y};
-            const iter = compute_point(point, cx, cy, maxiter, threshold);
-
-            if (iter == maxiter) {
-                // if we didn't get to infinity by the time we
-                // used up all the iterations, then we're in the set
-                // colour it bloack
-                ctx.fillStyle = "#000000";
-            } else {
-                // color it depending on the angle of alpha
-                const alpha = Math.atan(point.y);
-                if ((alpha >= 0) && (alpha <= 3)) {
-                    ctx.fillStyle = "#fff";
-                } else {
-                    ctx.fillStyle = "#000";
-                }
-            }
+            const iter = compute_point(point, cx, cy, max_iters, threshold);
+            set_colour_func(iter, max_iters, ctx, point);
 
             ctx.fillRect(ix, iy, 1, 1);
         }
@@ -245,7 +240,12 @@ function key_command_processor(e) {
     const evtobj = window.event ? event : e; //distinguish between IE's explicit event object (window.event) and Firefox's implicit.
     const unicode = evtobj.charCode ? evtobj.charCode : evtobj.keyCode;
     const actualkey = String.fromCharCode(unicode);
-    if (actualkey === "z") {
-        alert("zoom mode - coming soon");
+    switch (actualkey) {
+        case 'z':
+            alert("zoom mode - coming soon");
+            break;
+        case 'c':
+            alert("colour cycling mode - coming soon");
+            break;
     }
 }
